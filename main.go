13c64d9b2cdbb8562d64e35f78163dbde792e7bb
BOUOUZA Faycal (P) <faycal.bououza@ratp.fr>
2023-12-01 12:11:42 +0100

    add CRUD api for account

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"github.com/fayca121/simplebank/api"
+	db "github.com/fayca121/simplebank/db/sqlc"
+	_ "github.com/lib/pq"
+	"log"
+)
 
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://postgres:rocks@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
 }

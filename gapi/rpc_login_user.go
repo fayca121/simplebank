237f30fa736fef59342e529c4aa8e474f63bf14a
BOUOUZA Faycal (P) <faycal.bououza@ratp.fr>
2023-12-06 17:05:50 +0100

    implement grpc reverse proxy to manage http/grpc requests

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -48,12 +48,13 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		return nil, status.Errorf(codes.Internal, "error when converting id : %s", err)
 	}
 
+	mtdt := server.extractMetadata(ctx)
 	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
 		ID:           id,
 		Username:     req.GetUsername(),
 		RefreshToken: refreshToken,
-		UserAgent:    "",
-		ClientIp:     "",
+		UserAgent:    mtdt.UserAgent,
+		ClientIp:     mtdt.ClientIP,
 		IsBlocked:    false,
 		ExpiresAt:    refreshPayload.ExpiredAt,
 	})
